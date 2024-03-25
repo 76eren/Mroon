1e1ba2f40e22ef0d1fd2fb51bdbd9664b1452c54
Eren <e.basaran2004@gmail.com>
2024-03-25 18:01:20 +0100

    Fix: removed camera snap

--- Assets/Scripts/Spawning/Checkpoint/Checkpoint.cs
+++ Assets/Scripts/Spawning/Checkpoint/Checkpoint.cs
@@ -6,7 +6,7 @@ public class Checkpoint : MonoBehaviour
 {
     public bool cameraFollow; // Determines whether the camera should continiously be following us or if it should be static
     public int level; 
-    public Vector2 cameraPosition; // The position out camera should be on (if the camera is static of course)
+    public Vector2 cameraPosition; // The position out camera should be on (if the camera is static)
     public Vector2 playerLocation; // The position our player should respawn at.
 
 
@@ -20,8 +20,10 @@ public class Checkpoint : MonoBehaviour
 
     // Level snapping
     // Thes cannot be public because then we'd need to change this for each gameobject
-    private float cameraSpeed = 6f; 
-    private float cameraSpeedIncrease = 1.025f;
+    private float cameraSpeed = 80f; 
+    // private float cameraSpeed = 6f; 
+
+    private float cameraSpeedIncrease = 70f;
     private float initialCameraSpeed;
     private GameObject player;
 
@@ -71,46 +73,68 @@ public class Checkpoint : MonoBehaviour
 
     private void Update() 
     {
+        // Sets fps to 60
+        // Application.targetFrameRate = 60;
+
         // We only want to run this if this really is the active checkpoint
         // We dont want multiple checkpoints messing with each other
-        // This is a very bad way of checking
-        // We should be comparing objects instead.
-        // But I guess this will suffice.
         if (CheckpointData.returnCheckpoint().location != playerLocation)
         {
             return;
         }
 
 
-        // If camera follow is off (so a static camera)
         if (!CheckpointData.returnCheckpoint().camerafollow && !stopMovingTheCamera) 
         {
-            Vector3 position = Vector3.MoveTowards(
-                Camera.main.transform.position,
-                new Vector3(cameraPosition.x, cameraPosition.y, -10f),
-                cameraSpeed * Time.deltaTime
-                );
-            
-            
-            Camera.main.transform.position = position;
-            cameraSpeed *= cameraSpeedIncrease;
-        
-            if ( (Vector2) Camera.main.transform.position == cameraPosition)
-            {
-                stopMovingTheCamera=true;
-                cameraSpeed = initialCameraSpeed;
-            }
-        
+            CameraFollowStaticCamera();
         }    
 
 
-        // if our camera follow is on (so not static)
-    if (CheckpointData.returnCheckpoint().camerafollow)
-    {            
-        Vector3 newPosition = Vector3.MoveTowards(Camera.main.transform.position
+        if (CheckpointData.returnCheckpoint().camerafollow)
+        {            
+            CameraFollowDynamicCamera();
+        }
+
+
+        if (Camera.main.orthographicSize != cameraSize) 
+        {
+            CameraSizeIncreaseDecrease();
+        }
+    }
+
+
+    void CameraFollowStaticCamera() {
+        // Makes the camera snap to the target position, kind of like in Celeste
+
+        Vector3 currentPosition = Camera.main.transform.position;
+        Vector3 targetPosition = new Vector3(cameraPosition.x, cameraPosition.y, -10f);
+        float step = cameraSpeed * Time.deltaTime;
+
+        Vector3 position = Vector3.MoveTowards(
+            currentPosition,
+            targetPosition,
+            step
+            );
+    
+        Camera.main.transform.position = position;
+
+        // Deze logica werkt niet, heb het dus uit gezet
+        // cameraSpeed = cameraSpeed * cameraSpeedIncrease * Time.deltaTime;
+    
+        if ( (Vector2) Camera.main.transform.position == cameraPosition)
+        {
+            print("STopping camera follow!!");
+            stopMovingTheCamera=true;
+            // cameraSpeed = initialCameraSpeed;
+        }
+    }
+
+    void CameraFollowDynamicCamera() {
+        Vector3 newPosition = Vector3.MoveTowards(
+        Camera.main.transform.position
         ,new Vector3(
-             player.transform.localPosition.x + cameraDistanceFromPlayer
-            , player.transform.localPosition.y+2.4f // What is this magic number???
+            player.transform.localPosition.x + cameraDistanceFromPlayer
+            , player.transform.localPosition.y+2.4f 
             , -10f) 
         , cameraSpeed * Time.deltaTime);
         Camera.main.transform.position = newPosition;
@@ -118,28 +142,25 @@ public class Checkpoint : MonoBehaviour
     }
 
 
-        // Makes our camera increase/decrease size
-        if (Camera.main.orthographicSize != cameraSize) 
+    void CameraSizeIncreaseDecrease() {
+        if (Camera.main.orthographicSize < cameraSize)
         {
-            if (Camera.main.orthographicSize < cameraSize)
+            float size = Camera.main.orthographicSize * (1 + cameraSizeSpeedPercentage);
+            Camera.main.orthographicSize = size;
+            if (Camera.main.orthographicSize >= cameraSize)
             {
-                float size = Camera.main.orthographicSize * (1 + cameraSizeSpeedPercentage);
-                Camera.main.orthographicSize = size;
-                if (Camera.main.orthographicSize >= cameraSize)
-                {
-                    Camera.main.orthographicSize = cameraSize;
-                }        
-            }
-            else 
+                Camera.main.orthographicSize = cameraSize;
+            }        
+        }
+        else 
+        {
+            float size = Camera.main.orthographicSize * (1 - cameraSizeSpeedPercentage);
+            Camera.main.orthographicSize = size;
+            if (Camera.main.orthographicSize <= cameraSize)
             {
-                float size = Camera.main.orthographicSize * (1 - cameraSizeSpeedPercentage);
-                Camera.main.orthographicSize = size;
-                if (Camera.main.orthographicSize <= cameraSize)
-                {
-                    Camera.main.orthographicSize = cameraSize;
-                }
+                Camera.main.orthographicSize = cameraSize;
             }
-
         }
     }
+
 }
